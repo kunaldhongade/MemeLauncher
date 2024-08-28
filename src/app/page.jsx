1b60a88f24610d8930d56ada2031801c42a61604
kunaldhongade <kunaldhongade1@gmail.com>
2024-08-28 13:24:06 +0530

    Adding meme launcher

---
+++ src/app/page.jsx
@@ -0,0 +1,33 @@
+"use client";
+
+import Navbar from "@/components/Navbar/Navbar";
+import { WavyBackground } from "@/components/ui/wavy-background";
+import Link from "next/link";
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="relative overflow-hidden">
+        <div className="container mx-auto hero-main h-full">
+          <WavyBackground>
+            <div className="flex h-full items-center">
+              <div className="max-w-3xl mx-auto text-center text-white">
+                <h1 className="text-7xl mb-3">Core Launcher</h1>
+                <p className="mb-3">
+                  Meme Launcher simplifies ERC20 token creation on open campus codex EDU chain,
+                  offering customizable tokens and a DeFi that reducing costs and technical barriers.
+                </p>
+                <Link href="/createtoken" className="button button-primary">
+                  Launch App
+                </Link>
+              </div>
+            </div>
+          </WavyBackground>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
